aeea5d2095dc6c68a55c45b6191dfde364e478c1
Stan Manilov <stanislav.manilov@gmail.com>
2016-11-21 14:43:18 +0000

    Complicate 121

--- 121.offsets.c
+++ 121.offsets.c
@@ -1,12 +1,17 @@
 // The simplest of programs that has a substantial loop. Each element of an
 // array is initialized to its index in the array.
 
-
-int main() {
+int main(int argc, char *argv[]) {
   const unsigned N = 1 << 5;
+  unsigned B[N];
   unsigned A[N];
+  B[0] = argc;
+  B[1] = (unsigned)argv;
 
   for (unsigned i = 0; i < N; ++i) {
     A[i] = i;
+    B[i] = i;
   }
+
+  return ((unsigned)&B[N - 1]) % 4;
 }
