9f4747c34e5f84721a2d01945269656c88af7f73
Stan Manilov <stanislav.manilov@gmail.com>
2016-11-21 10:17:24 +0000

    Add 111 121 and 401

--- 111.argc.c
+++ 111.argc.c
@@ -1,12 +1,13 @@
 // The simplest of programs that has a substantial loop. Each element of an
 // array is initialized to its index in the array.
 
-
-int main() {
+int main(int argc, char *_[]) {
   const unsigned N = 1 << 5;
   unsigned A[N];
 
   for (unsigned i = 0; i < N; ++i) {
     A[i] = i;
   }
+
+  return argc + A[0];
 }
