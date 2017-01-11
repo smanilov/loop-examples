5225e560d73a3df79b0c388290b56bc82609861d
Stan Manilov <stanislav.manilov@gmail.com>
2017-01-11 16:13:26 +0000

    Add descriptory comment to specimen4 example

--- specimen4/main.cpp
+++ specimen4/main.cpp
@@ -1,4 +1,19 @@
+// Let's call this CFG the 'butterfly'.
+// It looks like this:
 //
+//     A
+// ,. / \,-,
+// | B1 B2 |
+// |  \ /  |
+// |   C   |
+// |  / \  |
+// | D1 D2 |
+// `-`\ /`-`
+//     E
+//
+// Where the edges between B1-D1 and B2-D2 are actually 'back-edges' which is to
+// say the direction is from D1 to B1 and from D2 to B2 (illustrated by edges
+// always leaving from the bottom of a node and entering through the top).
 #include <stdio.h>
 
 int main() {
@@ -12,26 +27,29 @@ int main() {
 
   int sum = 0;
   int i = 0;
-L0:
+A:
   if (A[i] % 2 == 0) {
-  left0:
+  B1:
     sum += A[i];
   } else {
-  right0:
+  B2:
     sum -= A[i];
   }
-  printf("%d", sum);
+C:
   if (A[i] % 2 == 0) {
+  D1:
     sum += A[i];
     if (++i < n) {
-      goto left0;
+      goto B1;
     }
   } else {
+  D2:
     sum -= A[i];
     if (++i < n) {
-      goto right0;
+      goto B2;
     }
   }
+E:
 
   printf("%d\n", sum);
   delete[] A;
