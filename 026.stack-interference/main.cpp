4f1df9e697e35cb20c1f2adff84b195195a4dec6
Stan Manilov <stanislav.manilov@gmail.com>
2017-06-21 17:43:35 +0100

    Add alias and dependence tests

--- 026.stack-interference/main.cpp
+++ 026.stack-interference/main.cpp
@@ -2,21 +2,21 @@
 
 int f() {
   int x, y[1024][1024];
-  scanf("%d", &x);            // hidden store
-  scanf("%d", &y[0][0]);      // hidden store
-  x += y[0][0];               // store 0, load 0, load 1 (WAR load 1 - store 0)
-  return x;                   // load 2                  (RAW: store 0)
+  scanf("%d", &x);       // hidden store
+  scanf("%d", &y[0][0]); // hidden store
+  x += y[0][0];          // store 0, load 0, load 1 (WAR load 1 - store 0)
+  return x;              // load 2                  (RAW: store 0)
 }
 
 int g() {
   int x, y[1024][1024];
-  scanf("%d", &x);            // hidden store
-  scanf("%d", &y[0][0]);      // hidden store
-  x += y[0][0];               // store 1, load 3, load 4 (WAR load 4 - store 1)
-                              //   *** interference: *** (RAW: store 0 - load 4)
-                              //   *** interference: *** (WAW: store 0 - store 1)
-                              //   *** interference: *** (WAR: load 2 - store 1)
-  return x;                   // load 5                  (RAW: store 1)
+  scanf("%d", &x);       // hidden store
+  scanf("%d", &y[0][0]); // hidden store
+  x += y[0][0];          // store 1, load 3, load 4 (WAR load 4 - store 1)
+                         //   *** interference: *** (RAW: store 0 - load 4)
+                         //   *** interference: *** (WAW: store 0 - store 1)
+                         //   *** interference: *** (WAR: load 2 - store 1)
+  return x;              // load 5                  (RAW: store 1)
 }
 
 int main() {
